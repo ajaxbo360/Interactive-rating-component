0e4204469b7118ae31e61c50d2eca87180036b78
ajaxbo360 <ajakak.2016@gmail.com>
2022-07-27 18:55:51 +0100

    adding the close modal button

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 
 
 const popup = document.querySelector("#pop-up");
+const closeBtn = document.querySelector(".thank-you__component__close__btn");
 
 let stars = document.querySelectorAll(".stars");
 const thank_you = document.querySelector(".thank-you__component");
@@ -10,8 +11,6 @@ function openPopup() {
     popup.classList.add("open-popup");
 }
 
-popup.addEventListener("click", function (e) {
-    if (popup.classList.contains("open-popup")){
-        popup.classList.remove("open-popup");
-    }
+closeBtn.addEventListener("click", function(e) {
+    popup.classList.remove("open-popup");
 })
